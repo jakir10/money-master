198e41c306bf84301eb2cb5f967a69662a1429b8
jakir10 <jakir.cse.bubt@gmail.com>
2022-02-17 21:21:19 +0600

    design updated & javascript code income part calculated

---
+++ js/app.js
@@ -0,0 +1,40 @@
+document.getElementById('expense-calculate').addEventListener('click', function () {
+
+    let getIncome = document.getElementById('income-input');
+
+    //income ballance
+    let incomeText = getIncome.value;
+    let totalIncome = parseFloat(incomeText);
+    // getIncome.value = '';
+
+
+    //all expense
+    const getFood = document.getElementById('food-input');
+    const getRent = document.getElementById('rent-input');
+    const getCloths = document.getElementById('cloths-input');
+
+    //convert input value into number
+    const foodText = getFood.value;
+    const foodExpense = parseFloat(foodText);
+
+    const rentText = getRent.value;
+    const rentExpense = parseFloat(rentText);
+
+    const clothsText = getCloths.value;
+    const clothsExpense = parseFloat(clothsText);
+    //get all expense calculation
+    const result = foodExpense + rentExpense + clothsExpense;
+
+    //get all expense result
+    const totalExpense = document.getElementById('total-expanse');
+    const expenseText = totalExpense.innerText;
+    totalExpense.innerText = result;
+
+    //clear input field
+    getFood.value = '';
+    getRent.value = '';
+    getCloths.value = '';
+
+})
+
+
